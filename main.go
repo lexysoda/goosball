e53683552ea89628f288c76f150c574f5a740f90
Roman Orzegowski <roman@mbr-targeting.com>
2022-02-18 21:22:53 +0100

    Clean up main

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	users := []string{"U02QK2J4BRD", "U02NPU059QT", "U029URUKJLF", "UB048064V"}
 	c := &controller.Controller{
 		Db:        db,
 		Elo:       goskill.New(),
@@ -27,29 +26,6 @@ func main() {
 		SlackHome: os.Getenv("SLACK_HOME_CHANNEL"),
 	}
 	_ = bot.New(c)
-	for _, id := range users {
-		u, err := c.GetOrCreateUser(id)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = c.AddToQueue(u.ID)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	for i := 0; i < 10; i++ {
-		c.Score(true)
-	}
-
-	for _, id := range users {
-		uGot, err := c.GetOrCreateUser(id)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("User: %s, Mu: %g, SigSq: %g\n", uGot.RealName, uGot.Skill.Mu, uGot.Skill.SigSq)
-	}
-
 	_ = api.New(c)
 	http.Handle("/api/", http.StripPrefix("/api", a))
 	http.Handle("/", http.FileServer(http.Dir("static")))
